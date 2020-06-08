e40047b0c37376052818b74919b0bed5933d3368
Matthew Barnhart <mcb4rnh4rt@gmail.com>
2020-06-08 17:18:42 -0500

    added Id to task model

--- ToDoTodayAPI/ToDoTodayAPI/Models/Task.cs
+++ ToDoTodayAPI/ToDoTodayAPI/Models/Task.cs
@@ -8,6 +8,8 @@ namespace ToDoTodayAPI.Models
 {
     public class Task
     {
+        public int Id { get; set; }
+
         [Required]
         public string Title { get; set; }
         public DateTime? StartTime { get; set; }
