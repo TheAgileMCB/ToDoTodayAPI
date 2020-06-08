d83b80d723180cb14cfac9c0270c7ac8c7cb1e76
Matthew Barnhart <mcb4rnh4rt@gmail.com>
2020-06-08 17:05:30 -0500

    added IdentityUser to ToDoUser model

--- ToDoTodayAPI/ToDoTodayAPI/Models/Identity/ToDoUser.cs
+++ ToDoTodayAPI/ToDoTodayAPI/Models/Identity/ToDoUser.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Identity;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
@@ -6,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace ToDoTodayAPI.Models
 {
-    public class ToDoUser
+    public class ToDoUser : IdentityUser
     {
         [Required]
         public string Name { get; set; }
