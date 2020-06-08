2de41f7e764dad50fc886d16589df2adcf931091
Matthew Barnhart <mcb4rnh4rt@gmail.com>
2020-06-08 16:33:19 -0500

    created TaskDTO

---
+++ ToDoTodayAPI/ToDoTodayAPI/Models/Api/TaskDTO.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ToDoTodayAPI.Models.Api
+{
+    public class TaskDTO
+    {
+        [Required]
+        public string Title { get; set; }
+        public string StartTime { get; set; }
+
+        public string DueTime { get; set; }
+
+        public string Description { get; set; }
+        public string EstimatedTimeToComplete { get; set; }
+    }
+}
