a758e96f7cd2b596a7d262b80365d8aebe37c671
Matthew Barnhart <mcb4rnh4rt@gmail.com>
2020-06-08 15:58:53 -0500

    created user model

---
+++ ToDoTodayAPI/ToDoTodayAPI/Models/User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ToDoTodayAPI.Models
+{
+    public class User
+    {
+        [Required]
+        public string Name { get; set; }
+        public string EyeColor { get; set; }
+        public string FavoriteFood { get; set; }
+    }
+}
