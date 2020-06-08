e5bbbf95ffa923dbca43b785763172063ec01297
Matthew Barnhart <mcb4rnh4rt@gmail.com>
2020-06-08 16:30:22 -0500

    added identityDBContext to UserDbContext

--- ToDoTodayAPI/ToDoTodayAPI/Data/UserDbContext.cs
+++ ToDoTodayAPI/ToDoTodayAPI/Data/UserDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -6,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace ToDoTodayAPI.Data
 {
-    public class UserDbContext : DbContext
+    public class UserDbContext : IdentityDbContext
     {
         public UserDbContext(DbContextOptions options) : base(options)
         {
