9bdb70cadbd1db535c575a380745fb9138557814
Matthew Barnhart <mcb4rnh4rt@gmail.com>
2020-06-08 16:45:33 -0500

    created UserDTO

---
+++ ToDoTodayAPI/ToDoTodayAPI/Models/Api/UserDTO.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ToDoTodayAPI.Models.Api
+{
+    public class UserDTO
+    {
+        [Required]
+        public string Name { get; set; }
+        public string EyeColor { get; set; }
+        public string FavoriteFood { get; set; }
+
+        public DateTime? Birthday { get; set; }
+    }
+}
