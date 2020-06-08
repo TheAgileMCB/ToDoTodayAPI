e9fea39903a117e6624e27248ca2c6938bd464b2
Matthew Barnhart <mcb4rnh4rt@gmail.com>
2020-06-08 15:56:01 -0500

    built Task List model

---
+++ ToDoTodayAPI/ToDoTodayAPI/Models/ToDoList.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ToDoTodayAPI.Models
+{
+    public class ToDoList
+    {
+        [Required]
+        public string Task { get; set; }
+        public string StartTime { get; set; }
+
+        public string DueTime { get; set; }
+
+        public string Description { get; set; }
+        public string EstimatedTimeToComplete { get; set; }
+    }
+}
--- ToDoTodayAPI/ToDoTodayAPI/obj/Debug/netcoreapp3.1/ToDoTodayAPI.AssemblyInfo.cs
+++ ToDoTodayAPI/ToDoTodayAPI/obj/Debug/netcoreapp3.1/ToDoTodayAPI.AssemblyInfo.cs
@@ -11,6 +11,7 @@
 using System;
 using System.Reflection;
 
+[assembly: Microsoft.Extensions.Configuration.UserSecrets.UserSecretsIdAttribute("d0f34d3f-e030-4cdf-bf05-158b9bd83800")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("ToDoTodayAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
