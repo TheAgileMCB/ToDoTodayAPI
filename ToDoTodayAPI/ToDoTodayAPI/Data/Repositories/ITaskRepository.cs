92e981ff16b08d5fe8e90189ed05f70718fed55d
Matthew Barnhart <mcb4rnh4rt@gmail.com>
2020-06-08 16:29:34 -0500

    built Task interface

---
+++ ToDoTodayAPI/ToDoTodayAPI/Data/Repositories/ITaskRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ToDoTodayAPI.Data.Repositories
+{
+    public interface ITaskRepository
+    {
+        Task<IEnumerable<TaskDTO>> GetAllTasks();
+
+        Task<TaskDTO> GetOneTask(int Id);
+
+        Task<bool> EditTask(int Id, Task task);
+
+        Task<Task> AddTask(Task task);
+
+        Task<Task> DeleteTask(int Id);
+    }
+}
