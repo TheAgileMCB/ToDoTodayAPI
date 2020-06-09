a383c546768750231691df9230ab8ff0dbedbca2
Matthew Barnhart <mcb4rnh4rt@gmail.com>
2020-06-09 11:23:12 -0500

    created and added Post RegisterData to usersController

---
+++ ToDoTodayAPI/ToDoTodayAPI/Controllers/UsersController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using ToDoTodayAPI.Models;
+
+namespace ToDoTodayAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly UserManager<ToDoUser> userManager;
+        {
+        this.userManager = userManager;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterData register)
+        {
+            var user = new ToDoUser
+            {
+                Email = register.Email,
+                UserName = register.Email,
+                FirstName = register.FirstName,
+                LastName = register.LastName
+            };
+
+            EnvironmentVariableTarget result = await UserManager.CreateAsync(user, register.Password);
+
+            if (!result.Succeeded)
+            {
+                return BadRequest(new
+                {
+                    message = "Registration Failed",
+                    errors = result.Errors,
+
+                });
+            }
+            return Ok(
+                        )
+    }
+    }
