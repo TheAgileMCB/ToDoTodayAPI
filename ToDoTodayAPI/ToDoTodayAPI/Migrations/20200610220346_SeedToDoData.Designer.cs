09864d4cd590f4e3a00b4d838047c50298835fc3
Matthew Barnhart <mcb4rnh4rt@gmail.com>
2020-06-10 17:04:28 -0500

    performed migration that seeded and named TaskItems db

--- ToDoTodayAPI/ToDoTodayAPI/Migrations/20200610220346_SeedToDoData.Designer.cs
+++ ToDoTodayAPI/ToDoTodayAPI/Migrations/20200610220346_SeedToDoData.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using ToDoTodayAPI.Data;
 
 namespace ToDoTodayAPI.Migrations
 {
     [DbContext(typeof(ToDoListDBContext))]
-    partial class ToDoListDBContextModelSnapshot : ModelSnapshot
+    [Migration("20200610220346_SeedToDoData")]
+    partial class SeedToDoData
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ ToDoTodayAPI/ToDoTodayAPI/Migrations/20200610220346_SeedToDoData.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ToDoTodayAPI.Migrations
+{
+    public partial class SeedToDoData : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "TaskItems",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(nullable: false),
+                    StartTime = table.Column<DateTime>(nullable: true),
+                    DueTime = table.Column<DateTime>(nullable: true),
+                    Assignee = table.Column<string>(nullable: true),
+                    Description = table.Column<string>(nullable: true),
+                    EstimatedTimeToComplete = table.Column<string>(nullable: true),
+                    DifficultyRating = table.Column<int>(nullable: false),
+                    CreatedByUserId = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TaskItems", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "TaskItems",
+                columns: new[] { "Id", "Assignee", "CreatedByUserId", "Description", "DifficultyRating", "DueTime", "EstimatedTimeToComplete", "StartTime", "Title" },
+                values: new object[] { 1, "Matthew", null, "Get up on that ladder and clean out those filthy gutters!", 4, new DateTime(2020, 6, 17, 17, 3, 45, 967, DateTimeKind.Local).AddTicks(3149), "2 hours", new DateTime(2020, 6, 10, 17, 3, 45, 963, DateTimeKind.Local).AddTicks(9613), "Clean Gutters" });
+
+            migrationBuilder.InsertData(
+                table: "TaskItems",
+                columns: new[] { "Id", "Assignee", "CreatedByUserId", "Description", "DifficultyRating", "DueTime", "EstimatedTimeToComplete", "StartTime", "Title" },
+                values: new object[] { 2, "Matthew", null, "Your plants are rootbound-- fix it!", 2, new DateTime(2020, 6, 17, 17, 3, 45, 967, DateTimeKind.Local).AddTicks(6547), "2 hours", new DateTime(2020, 6, 10, 17, 3, 45, 967, DateTimeKind.Local).AddTicks(6517), "Pot Plants" });
+
+            migrationBuilder.InsertData(
+                table: "TaskItems",
+                columns: new[] { "Id", "Assignee", "CreatedByUserId", "Description", "DifficultyRating", "DueTime", "EstimatedTimeToComplete", "StartTime", "Title" },
+                values: new object[] { 3, "Jessie", null, "The hard part is done. Now we just have to build and hang the panels.", 3, new DateTime(2020, 6, 17, 17, 3, 45, 967, DateTimeKind.Local).AddTicks(6604), "2 hours", new DateTime(2020, 6, 10, 17, 3, 45, 967, DateTimeKind.Local).AddTicks(6599), "Build the Fence" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "TaskItems");
+        }
+    }
+}
