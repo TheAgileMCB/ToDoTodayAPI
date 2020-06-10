9c28baecfe8013d50b176d7b603ef228706a36fe
Matthew Barnhart <mcb4rnh4rt@gmail.com>
2020-06-10 14:07:01 -0500

    connected Task Repo and continued building Controller logic

--- ToDoTodayAPI/ToDoTodayAPI/Controllers/TasksController.cs
+++ ToDoTodayAPI/ToDoTodayAPI/Controllers/TasksController.cs
@@ -1,9 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using ToDoTodayAPI.Data.Repositories;
+using ToDoTodayAPI.Models.Api;
 
 namespace ToDoTodayAPI.Controllers
 {
@@ -11,5 +15,54 @@ namespace ToDoTodayAPI.Controllers
     [ApiController]
     public class TasksController : ControllerBase
     {
+        private ITaskRepository _tasks;
+
+        public object ClaimType { get; private set; }
+
+        public TasksController(ITaskRepository tasks)
+        {
+            _tasks = tasks;
+        }
+
+        [HttpGet]
+        public async Task<List<TaskDTO>> Get()
+        {
+            return await _tasks.GetAllTasks();
+        }
+
+        [Authorize]
+        [HttpGet ("myTasks")]
+        public async Task<List<TaskDTO>> GetMyTasks()
+        {
+            return await _tasks.GetAllMyTasks(GetUserId());
+        }
+
+        [HttpGet("{id}")]
+        public async Task<TaskDTO> Get(int id)
+        {
+            return await _tasks.GetOneTask(id);
+        }
+
+        [Authorize]
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody] Task task)
+        {
+            task.CreatedByUserId = GetUserId();
+
+            await _tasks.AddTask(task);
+
+            return Ok("Complete!");
+        }
+
+        [HttpDelete("{id}")]
+        public async Task Delete(int id)
+        {
+            await _tasks.DeleteTask(id);
+        }
+
+        private string GetUserId()
+        {
+            return ((ClaimsIdentity)User.Identity).FindFirst(ClaimTypes.NameIdentifier).Value;
+        }
     }
 }
