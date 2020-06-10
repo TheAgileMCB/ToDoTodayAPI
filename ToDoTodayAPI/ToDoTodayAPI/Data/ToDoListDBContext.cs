fa95815b894b3e310b3e7b35e12eb72394b1f3c6
Matthew Barnhart <mcb4rnh4rt@gmail.com>
2020-06-10 17:02:34 -0500

    seeded the TaskItems DB

--- ToDoTodayAPI/ToDoTodayAPI/Data/ToDoListDBContext.cs
+++ ToDoTodayAPI/ToDoTodayAPI/Data/ToDoListDBContext.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ToDoTodayAPI.Models;
 
 namespace ToDoTodayAPI.Data
 {
@@ -13,6 +14,45 @@ namespace ToDoTodayAPI.Data
 
         }
 
-        // public DbSet<Task> Task { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<TaskItem>().HasData(
+               new TaskItem
+               {
+                   Id = 1,
+                   Title = "Clean Gutters",
+                   StartTime = DateTime.Now,
+                   DueTime = DateTime.Now.AddDays(7),
+                   Assignee = "Matthew",
+                   Description = "Get up on that ladder and clean out those filthy gutters!",
+                   EstimatedTimeToComplete = "2 hours",
+                   DifficultyRating = 4,
+               },
+               new TaskItem
+               {
+                   Id = 2,
+                   Title = "Pot Plants",
+                   StartTime = DateTime.Now,
+                   DueTime = DateTime.Now.AddDays(7),
+                   Assignee = "Matthew",
+                   Description = "Your plants are rootbound-- fix it!",
+                   EstimatedTimeToComplete = "2 hours",
+                   DifficultyRating = 2,
+               },
+               new TaskItem
+               {
+                   Id = 3,
+                   Title = "Build the Fence",
+                   StartTime = DateTime.Now,
+                   DueTime = DateTime.Now.AddDays(7),
+                   Assignee = "Jessie",
+                   Description = "The hard part is done. Now we just have to build and hang the panels.",
+                   EstimatedTimeToComplete = "2 hours",
+                   DifficultyRating = 3,
+               }
+               );
+        }
+
+        public DbSet<TaskItem> TaskItems { get; set; }
     }
 }
