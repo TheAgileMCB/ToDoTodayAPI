b65efbeb049fe5a89e5493738db7279564782346
Matthew Barnhart <mcb4rnh4rt@gmail.com>
2020-06-10 15:18:36 -0500

    implemented Task interface

---
+++ ToDoTodayAPI/ToDoTodayAPI/Data/Repositories/TaskRepository.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using ToDoTodayAPI.Models.Api;
+using Task = ToDoTodayAPI.Models.Task;
+
+namespace ToDoTodayAPI.Data.Repositories
+{
+    public class TaskRepository : ITaskRepository
+    {
+        public Task<Task> AddTask(Task task)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Task> DeleteTask(int Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> EditTask(int Id, Task task)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<TaskDTO>> GetAllMyTasks(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<TaskDTO>> GetAllTasks()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<TaskDTO> GetOneTask(int Id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
